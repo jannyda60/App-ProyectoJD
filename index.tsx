f667edd4d1fe8c67baf04455f93811d977a9de7a
jannyda60 <jannydasilva048@gmail.com>
2025-07-24 14:40:09 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>¡Hola desde React!</h1> {/* Un simple encabezado */}
   </React.StrictMode>
 );
